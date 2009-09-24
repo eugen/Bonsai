05c1a803cd900fb97fde9dd6e098f970ddc9c101
Eugen Anghel <eugen@syntactic.org>
2009-09-24 23:22:39 +0300

    added ClrIntegrationTests; they are all failing for now

---
+++ src/Tests/ClrIntegrationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class ClrIntegrationTests : BonsaiTestClass {
+        [TestMethod]
+        public void TestCallStaticProperty() {
+            Assert.AreEqual(
+                Environment.CurrentDirectory,
+                Execute("import .env .System.Environment \n env .CurrentDirectory"));
+        }
+        [TestMethod]
+        public void TestCallStaticMethod() {
+            Assert.AreEqual(
+                Environment.GetEnvironmentVariable("PATH"),
+                Execute("import .env .System.Environment \n env .GetEnvironmentVariable \"PATH\""));
+        }
+
+        [TestMethod]
+        public void TestInstantiateClass() {
+            Assert.AreEqual(
+                0,
+                Execute("import .int .System.Int32 \n int .new"));
+        }
+
+        [TestMethod]
+        public void TestCallInstanceProperty() {
+            Assert.AreEqual(
+                0,
+                Execute("import .list .System.Collections.ArrayList \n = .l (.list .new) \n l .Count"));
+        }
+
+        [TestMethod]
+        public void TestCallInstanceMethod() {
+            Assert.AreEqual(
+                typeof(System.Collections.ArrayList),
+                Execute("import .list .System.Collections.ArrayList \n = .l (.list .new) \n l .GetType"));
+        }
+    }
+}
