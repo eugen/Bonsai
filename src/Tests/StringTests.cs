031cdf5d64c2e51201216d7014fa4dde38418f21
Eugen Anghel <eugen@syntactic.org>
2009-10-10 20:27:34 +0300

    added some more string tests

--- src/Tests/StringTests.cs
+++ src/Tests/StringTests.cs
@@ -8,8 +8,13 @@ namespace Tests {
     [TestClass]
     public class StringTests : BonsaiTestClass {
         [TestMethod]
-        public void CallStringFrameworkMethodsTest() {
-            Assert.AreEqual("CAPS", Execute("\"caps\" .ToUpper"));
+        public void TestStringMethods() {
+            Assert.AreEqual("ZOMG", Execute("\"zomg\" .upcase"));
+            Assert.AreEqual("zomg", Execute("\"ZOMG\" .downcase"));
+            Assert.AreEqual("Omg Wtf Bbq", Execute("\"omg wtf bbq\" .capitalize"));
+
+            Assert.AreEqual(true, Execute("\"omgwtfbbq\" .contains \"wtf\""));
+            Assert.AreEqual(false, Execute("\"omgwtfbbq\" .contains \"bogus\""));
         }
     }
 }
