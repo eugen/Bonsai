8ceddeb33273e42cd2f783020312f405972b00f9
Eugen Anghel <eugen@syntactic.org>
2009-10-11 16:52:08 +0300

    defined some string primitives

--- src/Runtime/Primitives/BonsaiStringFunction.cs
+++ src/Runtime/Primitives/BonsaiStringFunction.cs
@@ -2,9 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Bonsai.Runtime.Primitives {
     public class BonsaiStringFunction : BonsaiPrimitiveFunction {
-
+        [MapsToSymbol("upcase")]
+        public string Upcase(string target, DictionaryBonsaiFunction scope, object[] args) {
+            return target.ToUpperInvariant();
+        }
+        [MapsToSymbol("downcase")]
+        public string Downcase(string target, DictionaryBonsaiFunction scope, object[] args) {
+            return target.ToLowerInvariant();
+        }
+        [MapsToSymbol("capitalize")]
+        public string Capitalize(string target, DictionaryBonsaiFunction scope, object[] args) {
+            return Regex.Replace(target, @"\b\w", new MatchEvaluator(m => m.Value.ToUpperInvariant()));
+        }
+        [MapsToSymbol("contains")]
+        public bool Contains(string target, DictionaryBonsaiFunction scope, object[] args) {
+            return target.Contains(((IConvertible)args[0]).ToString());
+        }
     }
 }
