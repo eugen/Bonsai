fff60265b60a3479492771e3931532e48ae5ca08
c15c8ra1n <eugen.anghel@softhouse-int.com>
2009-12-14 18:23:18 +0200

    added the first primitive for IList<T>

---
+++ src/Runtime/Primitives/BonsaiPrimitives.IList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace Bonsai.Runtime.Primitives {
+    public partial class BonsaiPrimitives {
+        [Primitive(typeof(IList<>), "contains")]
+        public static bool IListContains<T>(IList<T> target, DictionaryBonsaiFunction scope, object[] args) {
+            Debug.Assert(args.Length == 1 && args[0] is T);
+            return target.Contains((T)args[0]);
+        }
+       
+    }
+}
